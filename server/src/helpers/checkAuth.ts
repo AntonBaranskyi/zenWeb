dec064e98b2336ebaaea0c1bf4cc1a703f9a3f0b
Anton Baranskyi <antonbaranskij12@gmail.com>
2024-05-02 14:12:45 +0300

    Added user registartion check

---
+++ server/src/helpers/checkAuth.ts
@@ -0,0 +1,36 @@
+import jwt from 'jsonwebtoken';
+
+import {
+  Request as ExpressRequest,
+  Response as ExpressResponse,
+  NextFunction as ExpressNext,
+} from 'express';
+
+export default (
+  req: ExpressRequest,
+  resp: ExpressResponse,
+  next: ExpressNext
+) => {
+  const token = req.headers.authorization || '';
+
+  if (token) {
+    try {
+      const decodedToken = jwt.verify(token, 'secret123');
+
+      console.log(decodedToken);
+      // @ts-ignore
+
+      req.body.userId = decodedToken.id;
+      next();
+    } catch (error) {
+      console.log(error);
+      return resp.status(403).json({
+        message: 'No permission',
+      });
+    }
+  } else {
+    return resp.status(403).json({
+      message: 'No permission',
+    });
+  }
+};
--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import { registerValidation } from '../validation/registerValidation.js';
 import userController from '../controllers/userController.js';
+import checkAuth from '../helpers/checkAuth.js';
 
 const router = express.Router();
 
 router.post('/register', registerValidation, userController.register);
 router.post('/login', userController.login);
+router.get('/me', checkAuth, userController.getMe);
 
 export default router;
