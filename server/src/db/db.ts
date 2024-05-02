c56d431ab5655edd1d10a11f157ef0868ab3628e
Anton Baranskyi <antonbaranskij12@gmail.com>
2024-05-02 18:10:45 +0300

    Fixed

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -1,11 +1,27 @@
 import { Sequelize } from 'sequelize';
 
+const database = process.env.DATABASE
+  ? process.env.DATABASE.toString()
+  : 'postgres';
+const username = process.env.BD_USERNAME || 'postgres';
+const password = process.env.PASSWORD
+  ? process.env.PASSWORD.toString()
+  : '12345';
+const host = process.env.HOST ? process.env.HOST.toString() : 'localhost';
+const port = process.env.DB_PORT
+  ? parseInt(process.env.DB_PORT.toString())
+  : 5432;
+
+console.log(username);
+
 export const sequelize = new Sequelize(
-  process.env.DATABASE || 'postgres',
-  process.env.USERNAME || 'postgres',
-  process.env.PASSWORD || '12345',
+  database,
+  username,
+  password,
+
   {
-    host: process.env.HOST || 'localhost',
+    host,
     dialect: 'postgres',
+    port,
   }
 );
