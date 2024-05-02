289d979393e3781e1ef23610303b0269848bedf7
Anton Baranskyi <antonbaranskij12@gmail.com>
2024-05-02 16:47:43 +0300

    Add variables

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -1,6 +1,11 @@
 import { Sequelize } from 'sequelize';
 
-export const sequelize = new Sequelize('postgres', 'postgres', '12345', {
-  host: 'localhost',
-  dialect: 'postgres',
-});
+export const sequelize = new Sequelize(
+  process.env.DATABASE || 'postgres',
+  process.env.USERNAME || 'postgres',
+  process.env.PASSWORD || '12345',
+  {
+    host: process.env.HOST || 'localhost',
+    dialect: 'postgres',
+  }
+);
