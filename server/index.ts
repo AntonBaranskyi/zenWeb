891d5bbf7a4f36de424c0735ca06ba02201d9a4e
Anton Baranskyi <antonbaranskij12@gmail.com>
2024-05-02 17:43:20 +0300

    DB checked

--- server/index.ts
+++ server/index.ts
@@ -4,15 +4,24 @@ import cors from 'cors';
 
 import userRouter from './src/routes/user.js';
 import dealsRouter from './src/routes/deals.js';
+import { sequelize } from './src/db/db.js';
 
 dotenv.config();
 
 const app: Application = express();
 const PORT = process.env.PORT || 3000;
 
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('DB CONNECTED');
+  })
+  .catch((err) => {
+    console.error('DB ERRROR : ', err);
+  });
+
 app.use(cors());
 app.use(express.json());
-
 
 app.use('/auth', userRouter);
 
